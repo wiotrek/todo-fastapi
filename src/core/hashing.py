2244fe1670ad639c89675ca06d9f0f59bb39073d
Wiotrek <wiotrek@localhost.localdomain>
2024-08-05 22:46:02 +0200

    #1 user can be sign in but mutation for logged user not works

--- src/core/hashing.py
+++ src/core/hashing.py
@@ -1,14 +1,16 @@
 from bcrypt import gensalt, hashpw, checkpw
 
+
 class Hasher:
     @staticmethod
     def verify_password(plain_password: str, hashed_password: any):
         plain_password_byte_enc: bytes = plain_password.encode('utf-8')
-        return checkpw(password=plain_password_byte_enc, hashed_password=hashed_password)
+        hashed_password_byte_enc: bytes = hashed_password.encode('utf-8')
+        return checkpw(password=plain_password_byte_enc, hashed_password=hashed_password_byte_enc)
 
     @staticmethod
     def get_password_hash(password):
         pwd_bytes = password.encode('utf-8')
         salt = gensalt()
         hashed_password = hashpw(password=pwd_bytes, salt=salt)
-        return hashed_password
+        return hashed_password.decode('utf-8')
