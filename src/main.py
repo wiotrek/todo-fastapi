a8169c5052a8660408a466cafdaa44fe2be10ea4
Wiotrek <wiotrek@localhost.localdomain>
2024-09-14 13:16:17 +0200

    add origin for angular app

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 import strawberry
 from strawberry.fastapi import GraphQLRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.db.session import Base, engine
 from src.core.config import settings
@@ -22,6 +23,20 @@ def startup():
 def shutdown():
     pass
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
 
